a340920544b64fab5a703f6c9272231a641bbd4e
cauã Guilherme da conceição Salmaso <104331576+caua-cyber@users.noreply.github.com>
2023-04-25 17:21:29 -0300

    Primeiro comando em Java *comentado
    Comando feito em java, referente a aula do dia 25/04/2023

---
+++ Main (1).java
@@ -0,0 +1,6 @@
+/* descrição: Ola mundo em java */
+class Main { // criando uma classe
+  public static void main(String[] args){ // instancia
+    System.out.println("ola querido professor");
+  }
+}
