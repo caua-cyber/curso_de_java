7d1544afdf40f238c546664aa906b25360f6eb67
cauã Guilherme da conceição Salmaso <104331576+caua-cyber@users.noreply.github.com>
2023-04-25 17:34:39 -0300

    novo codigo
    codigo complementar com a estrutura de repetição for

---
+++ Main (2).java
@@ -0,0 +1,9 @@
+/* descrição: Ola mundo em java */
+class Main { // criando uma classe
+  public static void main(String[] args){ // instancia
+    System.out.println("ola querido professor");
+    for(int n = 0; n<=10;n++){
+      System.out.println(n); //impressao na tela
+    }
+  }
+}
