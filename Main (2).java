d3bd789d0573c0140fd06eaa9e133eacef3ac345
Guide_Code_developer <104331576+caua-cyber@users.noreply.github.com>
2023-05-17 16:04:48 -0300

    Delete Main (2).java

--- Main (2).java
+++
@@ -1,9 +0,0 @@
-/* descrição: Ola mundo em java */
-class Main { // criando uma classe
-  public static void main(String[] args){ // instancia, funcao publica que "nao devolve" valores "VOID"
-    System.out.println("ola querido professor");
-    for(int n = 0; n<=10;n++){
-      System.out.println(n); //impressao na tela
-    }
-  }
-}
